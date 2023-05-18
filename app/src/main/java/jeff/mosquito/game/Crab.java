414a51c7d02f42dde016e97dce1f84f26da8ce8a
OfficerGreg <Jakub.Gancarz@protonmail.com>
2023-05-18 21:29:21 +0200

    added color anim to crab based on time

--- app/src/main/java/jeff/mosquito/game/Crab.java
+++ app/src/main/java/jeff/mosquito/game/Crab.java
@@ -1,9 +1,13 @@
 package jeff.mosquito.game;
+import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
 import android.content.Context;
+import android.graphics.Color;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import java.util.Random;
@@ -37,6 +41,26 @@ public class Crab {
         imageView.setY(topMargin);
 
         shakeAnimation = AnimationUtils.loadAnimation(context, R.anim.shake_animation);
+
+        int startColor = Color.WHITE;
+        int endColor = Color.RED;
+
+        ValueAnimator colorAnimator = ValueAnimator.ofObject(new ArgbEvaluator(), startColor, endColor);
+        colorAnimator.setDuration(5000);
+        colorAnimator.setInterpolator(new LinearInterpolator());
+        colorAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        colorAnimator.setRepeatMode(ValueAnimator.REVERSE);
+
+        colorAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animator) {
+                int animatedColor = (int) animator.getAnimatedValue();
+                imageView.setColorFilter(animatedColor);
+            }
+        });
+
+        // Start the color animation
+        colorAnimator.start();
 
         layout.addView(imageView);
         crabLifespan = System.currentTimeMillis();
